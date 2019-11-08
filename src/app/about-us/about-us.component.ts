b26fed4be09b746846ee60fce9222767160b4a0e
DebjaniRoy <roy.debjani17@gmail.com>
2019-11-08 14:17:49 +0100

    adding images to about us page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -15,29 +15,30 @@ export class AboutUsComponent implements OnInit {
       this.cars=[
         {
         brand:'Design & Engineering Consultancy',
-        image: 'design_eng'
-      },
-      {
-        brand:' Design Validation as per code requirements',
-        image: 'design_eng'
+        image: 'design_eng.jpg'
       },
       {
         brand:' Feasibility study',
-        image: 'design_eng'
+        image: 'image_1.png'
       },
       {
+        brand:' Design Validation as per code requirements',
+        image: 'service_5.png'
+      },
+      
+      {
         brand:' Material Handling Planning',
-        image: 'design_eng'
+        image: 'service_4.jpg'
       },
       {
         brand:' Fault Finding',
-        image: 'design_eng'
+        image: 'image_2.jpg'
       },{
         brand:' Solid Modelling',
-        image: 'design_eng'
+        image: 'image_3.jpeg'
       },{
         brand:' Detail Engineering',
-        image: 'design_eng'
+        image: 'image_4.jpg'
       }]
         
     }
